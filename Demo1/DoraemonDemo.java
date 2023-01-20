5c8e72875f77e96d24a1f805e39a486a0148db4c
justbest2003 <justbest2003@gmail.com>
2023-01-20 12:44:50 +0700

    12.44

--- Demo1/DoraemonDemo.java
+++ Demo1/DoraemonDemo.java
@@ -10,5 +10,15 @@ public class DoraemonDemo {
         JOptionPane.showMessageDialog(null, "What can I do for you?", "Doraemon", JOptionPane.QUESTION_MESSAGE);
         JOptionPane.showMessageDialog(null, "I want to go to the future.", "Me", JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null, "Of course, what is the present year?", "Doraemon", JOptionPane.QUESTION_MESSAGE);
+
+        String nowyear = JOptionPane.showInputDialog(null, "It is:", "Me", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "OK. How many years do you want to travel?", "Doraemon", JOptionPane.QUESTION_MESSAGE);
+
+        String futureyear = JOptionPane.showInputDialog(null, "I want to go for:", "Me", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "OK. Lets goooooooooo!", "Doraemon",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        int allfuture = Integer.parseInt(nowyear) + Integer.parseInt(futureyear);
+        JOptionPane.showMessageDialog(null, "Hello welcome to " + allfuture, "Doraemon",JOptionPane.INFORMATION_MESSAGE);
     }
 }
