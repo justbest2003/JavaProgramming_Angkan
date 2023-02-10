6767edd6f01465053907b79285f2eead51192002
justbest2003 <justbest2003@gmail.com>
2023-02-10 10:40:19 +0700

    update

--- Exercise/Exercise21.java
+++ Exercise/Exercise21.java
@@ -1,9 +1,10 @@
+
 /**
 *Exercise21
 */
 import java.util.Scanner;
 
-public class Exercise21{
+public class Exercise21 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("number 1: ");
@@ -11,10 +12,10 @@ public class Exercise21{
         System.out.print("number 2: ");
         int b = input.nextInt();
         input.close();
-        System.out.println(a+" + "+b+" = "+(a+b));
-        System.out.println(a+" - "+b+" = "+(a-b));
-        System.out.println(a+" x "+b+" = "+(a*b));
-        System.out.println(a+" / "+b+" = "+(a/b));
-        System.out.println(a+" % "+b+" = "+(a%b));
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " x " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
     }
 }
