f002907f6b490ea047b00ae236d0de7a2d8557c9
justbest2003 <justbest2003@gmail.com>
2023-03-17 10:30:11 +0700

    update Ex58

--- Exercise5/Exercise58.java
+++ Exercise5/Exercise58.java
@@ -2,7 +2,7 @@ public class Exercise58 {
     public static void main(String[] args) {
         String ans = "";
         for (int i = 100; i < 1000; i++) {
-            if (i % 5 == 0 || i % 11 == 0 && i % 55 != 0) {
+            if ((i % 5 == 0 || i % 11 == 0) && i % 55 != 0) {
                 if (ans == "") {
                     ans = ans + i;
                 } else {
@@ -11,7 +11,7 @@ public class Exercise58 {
 
             }
         }
-        System.out.println("จำนวนเต็ม 3 หลัก ที่หารด้วย 5 หรือ 11 ลงตัวแต่หาร 55 ไม่ลงตัวได้แก่");
+        System.out.println("จำนวนเต็มบวก 3 หลัก ที่หารด้วย 5 หรือ 11 ลงตัวแต่หาร 55 ไม่ลงตัวได้แก่");
         System.out.println(ans);
     }
 }
